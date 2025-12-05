435937c27f390951a5578fd4f14b411bf322bd5a
Tran Huu Cuong <bambootran89@gmail.com>
2025-12-05 10:56:24 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# packagename
+
+## Setup
+### Requirements
+* python 3.11
+### Setup the environment and python packages
+```shell
+make venv PYTHON=python3.11
+```
+### Data
+Get data from telegram channel: `[DEV/DS/IC] packagename`.
+
+## Project Commands
+### Quick start
+Updating...
+
+### Run unit test
+```shell
+make test
+```
+### Run styling
+```shell
+make style
+```
---
+++ setup.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+# Load packages from requirements.txt
+BASE_DIR = Path(__file__).parent
+with open(Path(BASE_DIR, "requirements.txt")) as file:
+    required_packages = [ln.strip() for ln in file.readlines()]
+
+style_packages = [
+    "black==23.3.0",
+    "flake8==6.0.0",
+    "mypy==1.8.0",
+    "isort==5.12.0",
+    "types-PyYAML==6.0.12.10",
+    "pre-commit==3.3.2",
+    "pylint==3.2.7"
+]
+
+test_packages = ["pytest==7.3.2"]
+
+
+with open("README.md") as f:
+    readme = f.read()
+
+with open("LICENSE") as f:
+    license = f.read()
+
+setup(
+    name="packagename",
+    version="0.1",
+    description="packagename does something",
+    long_description=readme,
+    author="VNPT@IC",
+    author_email="",
+    license=license,
+    packages=find_packages(exclude=("tests", "docs")),
+    python_requires=">=3.11",
+    extras_require={
+        "dev": required_packages + style_packages + test_packages,
+        "test": required_packages + test_packages,
+    },
+)
